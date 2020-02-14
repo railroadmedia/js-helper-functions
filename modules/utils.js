2c138d69e0b122c9b1ac2a2bad89e44dad833c36
calebfavor <caleb@drumeo.com>
2020-02-14 09:07:06 -0800

    Fixing Taiwan country

--- modules/utils.js
+++ modules/utils.js
@@ -293,7 +293,7 @@ export default {
             {name: 'Sweden', code: 'SE'},
             {name: 'Switzerland', code: 'CH'},
             {name: 'Syrian Arab Republic', code: 'SY'},
-            {name: 'Taiwan, Province of China', code: 'TW'},
+            {name: 'Taiwan', code: 'TW'},
             {name: 'Tajikistan', code: 'TJ'},
             {name: 'Tanzania, United Republic of', code: 'TZ'},
             {name: 'Thailand', code: 'TH'},
